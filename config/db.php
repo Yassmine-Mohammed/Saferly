ed48e917f9d48af58abbc54435faa20f15f8db3a
Yasmine <magdyyasmin139@gmail.com>
2026-08-04 04:41:33 +0300

    Add booking feature and updates

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+
+    $con = mysqli_connect(
+        "db62034.public.databaseasp.net",
+        "db62040",
+        "Yk3@?6Em2h-W",
+        "db62040",
+        3306
+    );
+
+    mysqli_set_charset($con, "utf8mb4");
+
+} catch (mysqli_sql_exception $e) {
+
+    die($e->getMessage());
+
+}
