d0dfe6f9f648c54f0d265de0e77e543e04a2f81b
ahmed mohmed <ahmedelhmesh24@gmail.com>
2026-08-06 23:25:41 +0300

    update

--- company/add_trip.php
+++ company/add_trip.php
@@ -113,10 +113,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_trip'])) {
 }
 
 $page_title = "Add New Trip";
-require_once "../includes/header.php";
 ?>
-<link rel="stylesheet" href="../includes/CSS/includes.css">
-<link rel="stylesheet" href="CSS/company.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($page_title) ?> | Saferly</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="CSS/company.css">
+</head>
+<body>
 
 <div class="company-wrapper">
 
--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Saferly</title>
 
-    <link rel="stylesheet" href="../assets/css/style.css">
+     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
