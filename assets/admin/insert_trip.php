8832b1d7373e06098836f39b43575e612111f9a0
Yassmine <yasmyn.ftha00671@compit.aun.edu.eg>
2026-08-06 20:42:00 +0300

    add admin login

---
+++ assets/admin/insert_trip.php
@@ -0,0 +1,52 @@
+<?php
+
+include("../config/db.php");
+
+
+$company_id = $_POST['company_id'];
+$trip_name = $_POST['trip_name'];
+$destination = $_POST['destination'];
+$price = $_POST['price'];
+$duration_days = $_POST['duration_days'];
+$start_date = $_POST['start_date'];
+
+$query = "
+INSERT INTO trips
+(
+company_id,
+trip_name,
+destination,
+price,
+duration_days,
+start_date,
+status,
+description,
+category,
+hotel_level,
+image
+)
+
+VALUES
+(
+'$company_id',
+'$trip_name',
+'$destination',
+'$price',
+'$duration_days',
+'$start_date',
+'active',
+'',
+'',
+'',
+''
+)
+";
+
+mysqli_query($con, $query);
+
+
+header("Location: manage_trips.php?success=trip_added");
+
+exit();
+
+?>
