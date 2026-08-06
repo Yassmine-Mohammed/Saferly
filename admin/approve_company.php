743add1467f68659c9592ea936acea88461b5e0e
Tony Tharwat <ts199075-dotcom>
2026-08-06 10:55:27 +0300

    add my admin changes

--- admin/approve_company.php
+++ admin/approve_company.php
@@ -1,13 +1,16 @@
 <?php
 
 include("../config/db.php");
+
 $id = $_GET['id'];
 
-$query = "UPDATE companies SET status='approved' WHERE company_id=$id";
+$query = "UPDATE companies
+SET status='approved'
+WHERE company_id=$id";
 
 mysqli_query($con, $query);
 
-header("Location: manage_companies.php?success=companies.php");
+header("Location: manage_companies.php?success=company_approved");
 
 exit();
 
